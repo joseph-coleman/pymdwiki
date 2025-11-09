9e8ab1ac81f1536c1b6109042c92ab634c548a9b
joseph-coleman <74995797+joseph-coleman@users.noreply.github.com>
2025-11-08 18:25:30 -0600

    update local run files to support configurable port with env

--- uv_run.bat
+++ uv_run.bat
@@ -1,2 +1,14 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem Load variables from .env
+for /f "usebackq tokens=1,2 delims==" %%a in (".env") do (
+    set %%a=%%b
+)
+
+rem Default PORT if not set
+if "%PORT%"=="" set PORT=8000
+
+
 cd app
-uv run uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+uv run uvicorn main:app --host 127.0.0.1 --port %PORT% --reload
--- uv_run.sh
+++ uv_run.sh
@@ -1,2 +1,8 @@
+set -a 
+source .env 2>/dev/null || echo ".env file not found, using defaults"
+set +a
+
+PORT="${PORT:-8000}"
+
 cd app
-uv run uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+uv run uvicorn main:app --host 0.0.0.0 --port "$PORT" --reload
